26a0b0d3dbaa8dc19bb0718da42e7a86e9edcc00
Arslaan <118288807+OfficialArslaanTV@users.noreply.github.com>
2024-01-12 09:10:55 +1300

    Update script.js

--- about-blank-cloaker/script.js
+++ about-blank-cloaker/script.js
@@ -1,23 +1,18 @@
 var urlObj = new window.URL(window.location.href);
-var url = prompt("Enter a URL here: ");
-if (url) {
-	var win;
-
-	playButton = document.getElementById("cloak-button");
-	playButton.onclick = function () {
-		if (win) {
-  		win.focus()
-  	} else {
-  		win = window.open();
-    	win.document.body.style.margin = "0";
-    	win.document.body.style.height = "100vh";
-      var iframe = win.document.createElement('iframe');
-      iframe.style.border = 'none';
-      iframe.style.width = '100%';
-      iframe.style.height = '100%';
-      iframe.style.margin = '0';
-      iframe.src = url;
-      win.document.body.appendChild(iframe);
-  }
-}
-}
+var win;
+playButton = document.getElementById("cloak-button");
+playButton.onclick = function () {
+	if (win) {
+		win.focus()
+	} else {
+		win = window.open();
+		win.document.body.style.margin = "0";
+		win.document.body.style.height = "100vh";
+		var iframe = win.document.createElement('iframe');
+		iframe.style.border = 'none';
+		iframe.style.width = '100%';
+		iframe.style.height = '100%';
+		iframe.style.margin = '0';
+		iframe.src = prompt("Enter a URL here.");
+		win.document.body.appendChild(iframe);
+	}
