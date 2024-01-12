2d6f5bdb558790e8b2616c50505566630b7bb46c
Arslaan <118288807+OfficialArslaanTV@users.noreply.github.com>
2024-01-12 20:07:19 +1300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ let promptWebsite = () => {
   document.body.appendChild(IFrameElement);
   void 0;
 };
+aboutButton = document.querySelector("#about-button");
+aboutButton.onclick = function () {
+  win = window.open();
+  win.document.write("<p>About Arslaan's Website.</p><br/><p>Arslaan's Website was created for Arslaan to host a simple page with some of his creations, such as bookmark scripts and more! Stay tuned as the site is being updated very frequently.</p>");
+};
